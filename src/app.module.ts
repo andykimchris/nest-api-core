0f17182f3f5b62a8bf5da5e4ce04253e5efc70e7
andykimchris <andrew.muchiri97@gmail.com>
2023-08-14 11:51:20 +0300

    chore: imports modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { AuthModule } from './auth/auth.module';
+import { DbPrismaModule } from './db-prisma/db-prisma.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, BookmarkModule, AuthModule, DbPrismaModule],
 })
 export class AppModule {}
