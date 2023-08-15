7875abcd72055fa496847031d70b12a6fc2a1bc2
andykimchris <andrew.muchiri97@gmail.com>
2023-08-15 16:17:08 +0300

    fix: impoets db service to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { DbPrismaModule } from 'src/db-prisma/db-prisma.module';
 
 @Module({
   controllers: [AuthController],
+  imports: [DbPrismaModule],
   providers: [AuthService],
 })
 export class AuthModule {
