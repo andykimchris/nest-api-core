95c6c412af49daf6482829c330528934f56b398e
andykimchris <andrew.muchiri97@gmail.com>
2023-08-15 17:52:00 +0300

    fix: imports JwtModule to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { DbPrismaModule } from 'src/db-prisma/db-prisma.module';
 
 @Module({
   controllers: [AuthController],
-  imports: [DbPrismaModule],
+  imports: [DbPrismaModule, JwtModule.register({})],
   providers: [AuthService],
 })
 export class AuthModule {
