ef779671ad4711da0ff5c9d17cdf444f7457dfda
andykimchris <andrew.muchiri97@gmail.com>
2023-08-15 20:02:14 +0300

    feat: creates passport strategry for our authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { DbPrismaModule } from 'src/db-prisma/db-prisma.module';
+import { JwtStrategy } from './strategy';
 
 @Module({
   controllers: [AuthController],
   imports: [DbPrismaModule, JwtModule.register({})],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {
   constructor(private authService: AuthService) {}
