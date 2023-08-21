59463e720124b2748ee5bc148d32d823b2121fe4
andykimchris <andrew.muchiri97@gmail.com>
2023-08-21 19:08:56 +0300

    chore: imports users/bookmark service & controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,22 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthModule } from './auth/auth.module';
 import { DbPrismaModule } from './db-prisma/db-prisma.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { BookmarkController } from './bookmark/bookmark.controller';
+import { BookmarkService } from './bookmark/bookmark.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    UserModule,
-    BookmarkModule,
     AuthModule,
     DbPrismaModule,
+    UserModule,
   ],
+  controllers: [UserController, BookmarkController],
+  providers: [UserService, BookmarkService],
 })
 export class AppModule {}
