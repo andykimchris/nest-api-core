0c0b62e14e210a5ad1d6188e50bada3ff3ee7b5a
andykimchris <andrew.muchiri97@gmail.com>
2023-08-21 19:22:09 +0300

    fix: removes prisma module as it was globalized

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { DbPrismaModule } from 'src/db-prisma/db-prisma.module';
 import { JwtStrategy } from './strategy';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
-  imports: [DbPrismaModule, JwtModule.register({})],
   providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {
